d0413e83ba3a2284d9175770caa30a414b7f1f96
Ronaldo <rnlprzp@gmail.com>
2024-07-16 02:14:44 -0500

    inciando proyecto

---
+++ src/main/java/com/mixidev/literalurachallenge/LiterAluraChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mixidev.literalurachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiterAluraChallengeApplication.class, args);
+    }
+
+}
